fdbddf9fd5322a5c70648810805c9053b0a19fad
norafk <noracai0625@gmail.com>
2024-04-07 19:56:52 -0400

    fdsfds

--- code/transformer.py
+++ code/transformer.py
@@ -190,7 +190,7 @@ class TransformerBlock(tf.keras.layers.Layer):
         residual = masked_att + inputs # self.add = tf.keras.layers.Add() tk
         normalized_masked_att = self.layer_norm(residual)
 
-        unmasked_att = self.self_context_atten(normalized_masked_att, context_sequence) # tk what does it mean, unmasked 
+        unmasked_att = self.self_context_atten(context_sequence, context_sequence, normalized_masked_att) # tk what does it mean, unmasked 
         unmasked_att += masked_att # adding the previous thing? tk
         unmasked_att = self.layer_norm(unmasked_att)
         
