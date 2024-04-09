1e26af371c2043945b9c743a2a4a3aac49718bf1
norafk <noracai0625@gmail.com>
2024-04-08 20:28:03 -0400

    sdfsd

--- code/decoder.py
+++ code/decoder.py
@@ -77,6 +77,7 @@ class TransformerDecoder(tf.keras.Model):
         # Define classification layer(s) (LOGIT OUTPUT)
         self.classifier = tf.keras.Sequential([
             tf.keras.layers.Dense(self.hidden_size, activation=tf.nn.leaky_relu), 
+            tf.keras.layers.Dense(128, activation='relu'), 
             tf.keras.layers.Dense(self.vocab_size)])
 
     def call(self, encoded_images, captions):
